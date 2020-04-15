0d1465372cc50dc3437d48de0a163b92bdebc474
Luthfi02 <luthfi.rz.lrz@gmail.com>
2020-04-15 18:36:15 +0700

    Kalkulator

---
+++ Kalkulator/src/framekalkulator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Luthfi RZ
+ */
+public class framekalkulator {
+    public static void main(String[] args) {
+        
+        kalkulator kalku = new kalkulator();
+        kalku.setTitle("Kalkulator");
+        kalku.setVisible(true);
+    }
+}
